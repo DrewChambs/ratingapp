145667279fcd0e8ff4b91943144db884efa2a665
Andrew Chambers <andrew.chambers.wlv@gmail.com>
2022-03-25 09:42:25 -0400

    Media query added, change CSS classes, added  JS funcionlity

--- ratingapp.js
+++ ratingapp.js
@@ -2,8 +2,14 @@
 
 // Select Numbers buttons
 const btnNum = document.querySelectorAll(".btnNumbers");
+// Submit button link
 const btnSubmit = document.querySelector(".btnSubmit");
+// Link to display rating choice
 const thankYou = document.querySelector(".app_holder-selection");
+// Link to thanks container
+const thanksContainer = document.querySelector(".thanks_main");
+// Link to main rating container
+const ratingContainer = document.querySelector(".rating_main");
 
 btnNum.forEach(item => {
   item.addEventListener("click", e => {
@@ -13,8 +19,9 @@ btnNum.forEach(item => {
     let rating = e.currentTarget.dataset.id;
     rateArray.push(rating);
     btnSubmit.addEventListener("click", () => {
-      // let thankmessage = "You selected";
       thankYou.innerHTML = `You selected ${rating} out of 5`;
+      thanksContainer.classList.add("show-thanks");
+      ratingContainer.classList.add("show-rating");
     });
   });
 });
